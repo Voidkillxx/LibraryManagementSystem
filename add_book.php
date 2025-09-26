c6c246f47b1180b2f25666c50bf51029f8970343
Ramon <ramoncarlospacilona22@gmail.com>
2025-09-26 17:20:24 +0800

    Borrow Book

--- add_book.php
+++
@@ -1,6 +0,0 @@
-<?php $conn = new mysqli("db", "root", "rootpassword", "library_db"); 
-    if ($conn->connect_error) { 
-        die("Connection failed: " . $conn->connect_error); 
-    } 
-    
-?>
